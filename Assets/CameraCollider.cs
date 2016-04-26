95a6c514f0508bba8c9458bddbad5326de862f4d
Miguel Figueiredo Marques <miguelkedroon@gmail.com>
2016-04-25 21:05:10 -0400

    Skybox added

---
+++ Assets/CameraCollider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraCollider : MonoBehaviour {
+
+   
+    void Start()
+    {
+       
+    }
+}
