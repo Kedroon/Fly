b4e04c72bc5b89ff92cc696c8f8723c9a5053944
Miguel Figueiredo Marques <miguelkedroon@gmail.com>
2016-04-30 09:02:09 -0400

    Animation of Vertical and Horizontal Angles working, and working spaceship model

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+
+    public float speed;
+   
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	
+    void Update() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+     
+        
+
+        if (moveHorizontal < 0 && transform.position.x <= -7f)
+        {
+            moveHorizontal = 0;
+        }
+        else if (moveHorizontal > 0 && transform.position.x >= 7f) {
+            moveHorizontal = 0;
+        }
+
+        if (moveVertical > 0 && transform.position.y >= 4.6f) {
+            moveVertical = 0;
+        }
+        else if (moveVertical < 0 && transform.position.y <= -3.45f)
+        {
+            moveVertical = 0;
+        }
+           
+           
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical,0.0f );
+        
+        transform.Translate(movement * Time.deltaTime * speed,Space.World);
+        
+    }
+}
---
+++ Assets/Scripts/RotateSpaceShip.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateSpaceShip : MonoBehaviour {
+
+    
+    public Animator animator;
+    public string direction;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float move = Input.GetAxis(direction);
+        
+
+        if (move == 0)
+        {
+            animator.SetBool("Idle", true);
+        }
+        else {
+            animator.SetBool("Idle", false);
+            
+        }
+
+        animator.SetFloat("Move", move);
+
+    }
+}
